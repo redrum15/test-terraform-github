4b20bed33dfb8d4caafd64d23b3a78c6cd9e4905
Daniel Alba <56451942+redrum15@users.noreply.github.com>
2025-04-11 16:45:19 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,3 +55,82 @@ resource "github_branch_protection" "protect_stage" {
   allows_force_pushes = false
   allows_deletions    = false
 }
+
+
+
+resource "aws_cloudfront_distribution" "test" {
+  default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+
+  custom_error_response {
+    error_caching_min_ttl = 10
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 10
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+      "PUT",
+    ]
+    cached_methods = [
+      "GET",
+      "HEAD",
+    ]
+    target_origin_id = aws_s3_bucket.b.bucket_regional_domain_name
+
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  origin {
+    domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = aws_s3_bucket.b.bucket_regional_domain_name
+  }
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+
+resource "aws_cloudfront_origin_access_control" "example" {
+  name                              = "example"
+  description                       = "Example Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
